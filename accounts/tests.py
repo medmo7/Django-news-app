4042d22c4d36e093eab82a0557bdfa16471c78a2
mohamed <bourga.mohamed@gmail.com>
2024-11-27 10:58:49 +0100

    added custom user model, and added basic user creations logic tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="testuser", email="testuser@example.com", password="testpass1234"
+        )
+
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_super_user(self):
+        User = get_user_model()
+        super_user = User.objects.create_superuser(
+            username="test_super", email="test_super@test.com", password="superpass1234"
+        )
+
+        self.assertEqual(super_user.username, "test_super")
+        self.assertEqual(super_user.email, "test_super@test.com")
+        self.assertTrue(super_user.is_active)
+        self.assertTrue(super_user.is_superuser)
+        self.assertTrue(super_user.is_staff)
--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,7 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# The user model to use
+AUTH_USER_MODEL = "accounts.CustomUser"
